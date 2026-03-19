a8968257d494347a4d5022020b403609a635c775
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-20 01:54:26 +0200

    fix Header to take in course.name instead of course

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -47,26 +47,26 @@ const App = () => {
   const course = {
     name: 'Half Stack application development',
     parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      {/* <Content parts={parts} />
+      <Total parts={parts} /> */}
     </div>
   )
 }
