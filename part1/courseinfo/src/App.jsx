1ebfa15bf680403926b693b373453146e9fd1097
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-19 02:17:39 +0200

    add Header component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,15 @@
+
+const Header = (props) => {
+
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -10,6 +22,7 @@ const App = () => {
   return (
     <div>
       <h1>{course}</h1>
+      <Header course={course} />
       <p>
         {part1} {exercises1}
       </p>
