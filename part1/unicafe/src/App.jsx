03fc15868ff51a0b6a3069ba65dea162925abc72
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-24 16:21:59 +0200

    add Button component to unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from 'react'
 
+
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodReview = () => {
+    setGood(good + 1)
+  }
 
   return (
     <div>
       <h1>give feedback</h1>
 
-      <button></button>
-      <button></button>
-      <button></button>
+      <Button onClick={handleGoodReview} text="good"/>
 
       <section>
         <h2>statistics</h2>
-        <p>good</p>
+        <p>good {good}</p>
         <p>neutral</p>
         <p>bad</p>
       </section>
