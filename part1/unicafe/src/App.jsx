448f0a529bfbd7ef3451c4c134765e24be33c448
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-24 20:49:25 +0200

    rm unused variable

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -64,7 +64,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
 
   const handleGoodReview = () => {
     setGood(good + 1)
