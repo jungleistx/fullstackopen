36915b4038bea8e3027d55f578a829615b6f48dc
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-24 16:24:46 +0200

    add Button for neutral and bad reviews in unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,17 +18,27 @@ const App = () => {
     setGood(good + 1)
   }
 
+  const handleNeutralReview = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadReview = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
 
       <Button onClick={handleGoodReview} text="good"/>
+      <Button onClick={handleNeutralReview} text="neutral"/>
+      <Button onClick={handleBadReview} text="bad"/>
 
       <section>
         <h2>statistics</h2>
         <p>good {good}</p>
-        <p>neutral</p>
-        <p>bad</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
       </section>
 
     </div>
