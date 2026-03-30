33ae5d6583ac1ee4362398274ddd21ef9dd51614
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-30 17:14:13 +0300

    refactor App to use Course

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from "./components/Course"
+
 
 const App = () => {
   const course = {
@@ -7,31 +9,23 @@ const App = () => {
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
         exercises: 14,
-      },
-    ],
+        id: 3
+      }
+    ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
-    </div>
-  )
+  return <Course course={course} />
 }
+
 
 export default App
