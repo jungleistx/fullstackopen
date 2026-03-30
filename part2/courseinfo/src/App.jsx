95d747816069c4ce50391d9fcc1dac304035f646
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-03-30 16:15:32 +0300

    add Content.jsx

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,13 +1,5 @@
 const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
-
 const Part = (props) => (
   <p>
     {props.part.name} {props.part.exercises}
---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,9 @@
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
+
+export default Content
