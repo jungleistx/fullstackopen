0a9911da8af159aaea5d09c5c7d981c60f45f9a0
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-04-04 02:56:53 +0300

    add onSubmit to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,10 +6,20 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
           name: <input />
         </div>
