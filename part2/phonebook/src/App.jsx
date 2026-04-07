b8b2b149eb02ddd184b0dccb7c7d0721e6216121
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-04-07 15:41:46 +0300

    add id and dummydata

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'
-     }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const [filteredPersons, setFilteredPersons] = useState([])
 
   const isNameInPersons = (name) => {
     if (persons.some(person => person.name === name)) {
@@ -26,7 +29,8 @@ const App = () => {
       else {
         const personObject = {
           name: newName,
-          number: newNumber
+          number: newNumber,
+          id: persons.length + 1
         }
         setPersons(persons.concat(personObject))
         setNewName('')
