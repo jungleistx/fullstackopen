649a958f52b3b6bd1d09362d37c1dab99c4910ba
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-04-17 13:41:04 +0300

    add Filter component

---
+++ part2/countries/src/components/Filter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Filter = ({ value, onChange }) => {
+  return (
+	<form>
+		find countries
+		<input
+			value={value}
+			onChange={onChange}
+		/>
+	</form>
+  )
+}
+
+export default Filter
