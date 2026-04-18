cc964df825dd69a6c2a7c2c27946582f060522b0
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-04-18 10:42:25 +0300

    add changes to App

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Filter from './components/Filter'
+import CountryService from './services/country'
+import Notification from './components/Notification'
+import SearchResults from './components/SearchResults'
+
 
 const App = () => {
+  const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [allCountries, setAllCountries] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([])
+
+
+  useEffect(() => {
+    CountryService
+      .getAll()
+      .then(countries => {
+        setAllCountries(countries)
+      })
+      .catch(error => {
+        setErrorMessage(`Error fetching countries`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 6000)
+      })
+  }, [])
+
+  const handleChange = (event) => {
+    const value = event.target.value
+    setFilter(value)
+    setFilteredCountries(allCountries.filter(c => (
+      c.name.common.toLowerCase().includes(value.toLowerCase())
+    )))
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <Notification message={errorMessage}/>
+
+      <Filter
+        value={filter}
+        onChange={handleChange}
+      />
+
+      <SearchResults
+        countries={filteredCountries}
+      />
+
+    </div>
   )
 }
 
