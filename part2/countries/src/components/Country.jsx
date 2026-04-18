8326d85e90c36e72a60e31a873dc4efdde0af8db
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-04-18 10:33:24 +0300

    add Country component

---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,22 @@
+const Country = ({ country }) => {
+	console.log('Country', country);
+
+  return (
+	<>
+	<h1>{country.name.common}</h1>
+	<div>Capital {country.capital}</div>
+	<div>Area {country.area}</div>
+
+	<h2>Languages</h2>
+	<ul>
+		{Object.values(country.languages).map(lang => (
+			<li key={lang}>{lang}</li>
+		))}
+	</ul>
+
+	<img width="200px" src={country.flags.png} alt={country.flags.alt} />
+	</>
+  )
+}
+
+export default Country
