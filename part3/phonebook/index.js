0edc44ec585133422cf66420deaf2d52b9e24f7c
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-04 18:49:55 +0300

    add cors

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,10 @@ http = require('http')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('person', (req) => {
   if (req.method === 'POST')
