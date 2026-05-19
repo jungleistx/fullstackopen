0ec6f2cd2c1063b877a0e20502d22aea0b037a9e
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-20 02:37:04 +0300

    fix lints, add lint script

--- part3/phonebook/models/people.js
+++ part3/phonebook/models/people.js
@@ -8,7 +8,7 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { family: 4 })
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -16,30 +16,30 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		minLength: 3,
-		required: true
-	},
-	number: {
-		type: String,
-		minLength: 8,
-		required: true,
-		validate: {
-			validator: function(v) {
-				return /\d{2,3}-\d+$/.test(v)
-			},
-			message: props => `${props.value} is not valid phone number!`
-		}
-	}
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not valid phone number!`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString()
-		delete returnedObject._id
-		delete returnedObject.__v
-	}
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const argvLen = process.argv.length
 
 if (argvLen < 3) {
-  console.log('usage:');
-  console.log('print phonebook: node mongo.js <password>');
-  console.log('add person: node mongo.js <password> <name> <number>');
+  console.log('usage:')
+  console.log('print phonebook: node mongo.js <password>')
+  console.log('add person: node mongo.js <password> <name> <number>')
   process.exit(1)
 }
 
@@ -16,37 +16,37 @@ const url =  `mongodb+srv://fso_phonebookuser:${password}@cluster0.85bumku.mongo
 mongoose.set('strictQuery',false)
 mongoose.connect(url, { family: 4 })
   .catch(error => {
-	console.log(`${error.message}`)
-	process.exit(1)
+    console.log(`${error.message}`)
+    process.exit(1)
   })
 
 const personSchema = new mongoose.Schema({
-	name: String,
-	number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (argvLen === 3) {
-	Person.find({}).then(results => {
-		console.log('phonebook:');
-		results.forEach(note => {
-			console.log(`${note.name} ${note.number}`)
-		})
-		mongoose.connection.close()
-	})
+  Person.find({}).then(results => {
+    console.log('phonebook:')
+    results.forEach(note => {
+      console.log(`${note.name} ${note.number}`)
+    })
+    mongoose.connection.close()
+  })
 }
 else if (argvLen === 5) {
-	const newName = process.argv[3]
-	const newNumber = process.argv[4]
+  const newName = process.argv[3]
+  const newNumber = process.argv[4]
 
-	const person = new Person({
-		name: newName,
-		number: newNumber,
-	})
+  const person = new Person({
+    name: newName,
+    number: newNumber,
+  })
 
-	person.save().then(result => {
-		console.log(`added ${newName} number ${newNumber} to phonebook`);
-		mongoose.connection.close()
-	})
+  person.save().then(() => {
+    console.log(`added ${newName} number ${newNumber} to phonebook`)
+    mongoose.connection.close()
+  })
 }
