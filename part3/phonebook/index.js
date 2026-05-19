77c501a0dcd74ef2ae8fdf37070d989292db667f
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-19 17:44:57 +0300

    add validation error to errorhandler

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -157,6 +157,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).json({ error: 'malformatted id' })
   }
+  else if (error.name === 'ValidationError') {
+	  return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
