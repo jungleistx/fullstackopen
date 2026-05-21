07ec4958e2879508ad2016d33237ea98889799f1
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-22 00:44:01 +0300

    refactor schema into models/blog.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,19 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
+const Blog = require('./models/blog')
 require('dotenv').config()
-
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
