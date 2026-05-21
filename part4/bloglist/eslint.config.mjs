eb5123f7ac4e7de57232dc3e8861a1d3d5d0d92e
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-21 14:18:16 +0300

    fix typos

--- part4/bloglist/eslint.config.mjs
+++ part4/bloglist/eslint.config.mjs
@@ -1,10 +1,3 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
 import globals from 'globals'
 import js from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin'
