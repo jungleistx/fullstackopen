05698a501f8f6a3e24f13175af5ccae29442b942
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-22 01:34:45 +0300

    add utils/config.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,19 +5,25 @@ const Blog = require('./models/blog')
 require('dotenv').config()
 const blogsRouter = require('./controllers/blogs')
 const app = express()
+const config = require('./utils/config')
 
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
+  .then(() => {
+    logger.info('connected to mongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to mongoDB', error.message)
+  })
+
 
 app.use(express.json())
-
-logger.info('connecting to', mongoUrl)
 
 app.use('/api/blogs', blogsRouter)
 
 
-const PORT = process.env.PORT || 3003
+const PORT = config.PORT || 3003
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
