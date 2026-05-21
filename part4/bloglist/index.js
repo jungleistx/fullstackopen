bc0e08974111e60aa4243b25af43c44128834a07
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-21 22:04:30 +0300

    add dotenv

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+require('dotenv').config()
+
 
 const app = express()
 
@@ -12,10 +15,12 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
+
+logger.info('connecting to', mongoUrl)
 
 app.get('/api/blogs', (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -31,7 +36,7 @@ app.post('/api/blogs', (request, response) => {
   })
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
