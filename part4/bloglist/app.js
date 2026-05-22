61c02fcfc12ac91b93c4f30c19180632f273d802
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-22 03:06:09 +0300

    add app.js, refactor index.js

---
+++ part4/bloglist/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+require('dotenv').config()
+const blogsRouter = require('./controllers/blogs')
+const app = express()
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to mongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to mongoDB', error.message)
+  })
+
+
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,30 +1,6 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-require('dotenv').config()
-const blogsRouter = require('./controllers/blogs')
-const app = express()
+const app = require('./app')
 const config = require('./utils/config')
-const middleware = require('./utils/middleware')
-
-mongoose.connect(config.MONGODB_URI, { family: 4 })
-  .then(() => {
-    logger.info('connected to mongoDB')
-  })
-  .catch(error => {
-    logger.error('error connecting to mongoDB', error.message)
-  })
-
-
-app.use(express.json())
-
-app.use(middleware.requestLogger)
-app.use('/api/blogs', blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
+const logger = require('./utils/logger')
 
 const PORT = config.PORT || 3003
 app.listen(PORT, () => {
