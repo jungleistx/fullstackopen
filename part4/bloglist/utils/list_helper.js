0e65960cef5be57724055eccc30e4392b306c958
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-23 20:39:42 +0300

    add favoriteblog test and function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  return blogs.reduce((max, blog) => {
+    return blog.likes > max.likes ? blog : max
+  })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
