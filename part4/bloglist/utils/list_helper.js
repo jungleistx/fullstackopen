4e24fad605fd954ca55eae1c4ae12e4f28ce0bdb
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-05-25 11:09:20 +0300

    add lodash, mostblogs tests and function

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,10 +1,15 @@
+const _ = require('lodash')
+
+
 const dummy = (blogs) => {
   return 1
 }
 
+
 const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
+
 
 const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return 0
@@ -14,8 +19,24 @@ const favoriteBlog = (blogs) => {
   })
 }
 
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0)
+    return null
+
+  const counts = _.countBy(blogs, 'author')
+  const topAuthor = _.maxBy(Object.keys(counts), author => counts[author])
+
+  return {
+    author: topAuthor,
+    blogs: counts[topAuthor]
+  }
+}
+
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
