99af06fac3c5bcbab29759d3f81ff14a993ab902
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-07-16 10:56:01 +0300

    fix blogs to be array in userschema

--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -9,10 +9,10 @@ const userSchema = new mongoose.Schema({
   },
   name: String,
   passwordHash: String,
-  blogs: {
+  blogs: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Blog'
-  }
+  }]
 })
 
 userSchema.set('toJSON', {
