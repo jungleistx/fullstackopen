d231173c98efeb7cfe8fe2c1a5c7b74560de30fe
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-07-21 10:47:51 +0300

    add login.test.js

---
+++ part4/bloglist/tests/login.test.js
@@ -0,0 +1,57 @@
+const assert = require('node:assert')
+const { test, after, beforeEach, describe } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const { resetUserDb } = require('./test_helper')
+
+const api = supertest(app)
+
+
+describe('login user', () => {
+
+  beforeEach(async () => {
+    await resetUserDb()
+  })
+
+
+  test('success root user with valid pw', async () => {
+    const result = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'mysterypw' })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    assert(result.body.token)
+    assert.strictEqual(result.body.username, 'root')
+  })
+
+
+  test('fails root user with wrong pw', async () => {
+    const result = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'wrong' })
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+
+    assert(!result.body.token)
+    assert(result.body.error.includes('invalid username or password'))
+  })
+
+
+  test('fails with nonexistent user', async () => {
+    const result = await api
+      .post('/api/login')
+      .send({ username: 'nouser', password: 'random' })
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+
+    assert(!result.body.token)
+    assert(result.body.error.includes('invalid username or password'))
+  })
+
+
+  after(async () => {
+    await mongoose.connection.close()
+  })
+})
