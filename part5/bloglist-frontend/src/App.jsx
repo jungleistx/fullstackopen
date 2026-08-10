3031e1ff22cf2e6c1b289bec5e4a7ad6d19cc77a
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-08-10 03:39:12 +0300

    add blogs when user logged in

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -49,6 +49,15 @@ const App = () => {
 
   return (
     <div>
+      {user &&
+        <>
+          <h2>blogs</h2>
+          <p>{user.username} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </>
+      }
 
       {!user &&
         <>
