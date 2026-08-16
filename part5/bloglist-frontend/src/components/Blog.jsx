636790436c2d481a5f73e307f106b0bbef753aa7
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-08-16 14:36:21 +0300

    add view button to Blog

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isShown, setIsShown] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const buttonText = isShown ? 'hide' : 'view'
+
+
+  return (
+    <>
+    {!isShown &&
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={() => setIsShown(!isShown)}>{buttonText}</button>
+        </div>
+      </div>
+    }
+
+    {isShown &&
+      <div style={blogStyle}>
+        <div>
+          <div>{blog.title}
+            <button onClick={() => setIsShown(!isShown)}>{buttonText}</button>
+          </div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}<button>like</button> </div>
+          <div>{blog.author}</div>
+        </div>
+      </div>
+    }
+    </>
+)}
 
 export default Blog
