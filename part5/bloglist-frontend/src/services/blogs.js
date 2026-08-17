fce656ad5c90602f5a2de1676ee1980d8b6a0a28
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-08-17 15:26:15 +0300

    add update to blogservice

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -21,4 +21,13 @@ const create = async newObject => {
   return response.data
 }
 
-export default { getAll, create, setToken }
+const update = async (blogObject) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.put(`${baseUrl}/${blogObject.id}`, blogObject, config)
+
+  return response.data
+}
+
+export default { getAll, create, setToken, update }
