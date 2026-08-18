156fda7182a3056a3916a644e281133f8286d0ea
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-08-18 10:54:51 +0300

    add deleteBlog to blogservice

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -30,4 +30,13 @@ const update = async (blogObject) => {
   return response.data
 }
 
-export default { getAll, create, setToken, update }
+const deleteBlog = async (blogObject) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.delete(`${baseUrl}/${blogObject.id}`, config)
+
+  return response.data
+}
+
+export default { getAll, create, setToken, update, deleteBlog }
