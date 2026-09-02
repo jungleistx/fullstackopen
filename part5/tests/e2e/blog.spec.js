e64df6e1fbcfddc774eb827b9e280b1f31023544
Roope Vuorenlehto <r.vuorenlehto@gmail.com>
2026-09-02 23:08:08 +0300

    add tests/e2e/blog.spec.js

---
+++ part5/tests/e2e/blog.spec.js
@@ -0,0 +1,18 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('has title', async({ page }) => {
+    await expect(page).toHaveTitle('Blog app')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByRole('heading', { name: 'Log in to the application' })).toBeVisible()
+    await expect(page.getByRole('textbox', { name: 'username' })).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'password' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
+  })
+})
